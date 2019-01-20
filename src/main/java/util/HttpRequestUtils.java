daec68c89a8f1fb625fbf0abca02e8b2cf1ffdfa
js <shk3029>
2019-01-20 14:27:29 +0900

    #20190120 Sunday 요구사항2,3,4

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -1,13 +1,25 @@
 package util;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Maps;
-
 public class HttpRequestUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(HttpRequestUtils.class);
+
+    public static String getUrl(String firstUrl) {
+        String[] tokens = firstUrl.split(" ");
+        String url = tokens[1];
+        log.debug("request path : {}", url);
+
+        return url;
+    }
     /**
      * @param queryString은
      *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,14 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,30 +29,66 @@ public class RequestHandler extends Thread {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in,"UTF-8"));
 
-
-            //String line = null;
             String line = bufferedReader.readLine();
             log.debug("request line : {}", line);
             if(line == null) return;
+            String url = HttpRequestUtils.getUrl(line);
 
-            String[] tokens = line.split(" ");
-            String url = tokens[1];
             byte[] body = null;
 
+            Map<String, String> headerMap = new HashMap<>();
+
             while(!line.equals("")) {
-                line = bufferedReader.readLine();
                 log.debug("header : {} ", line );
+                line = bufferedReader.readLine();
+                String[] headerTokens = line.split(":");
+                if(headerTokens.length == 2) {
+                    headerMap.put(headerTokens[0], headerTokens[1].trim());
+                }
+            }
+        /*
+            // Get 방식
+            if(url.startsWith("/user/create?")) {
+                int index = url.indexOf("?");
+                String requestUrl =  url.substring(0, index);
+                String paramUrl =  url.substring(index+1);
+                Map<String, String> map = HttpRequestUtils.parseQueryString(paramUrl);
+                User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
+                log.debug("requestUrl : {}", requestUrl);
+                log.debug("paramUrl : {}", paramUrl);
+                log.debug("User Info : {}", user);
+                url = "/index.html";
+            }
+        */
+
+            // Post 방식
+            if(url.startsWith("/user/create")) {
+                String postRequestBody = IOUtils.readData(bufferedReader, Integer.parseInt(headerMap.get("Content-Length")));
+                log.debug("readData : {}", postRequestBody);
+                Map<String, String> map = HttpRequestUtils.parseQueryString(postRequestBody);
+                User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
+                log.debug("user : {}", user);
+
+                DataOutputStream dos = new DataOutputStream(out);
+                response302Header(dos);
+            } else {
+                DataOutputStream dos = new DataOutputStream(out);
+                body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             }
 
-            DataOutputStream dos = new DataOutputStream(out);
-            if(url.equals("/")) {
-                body = "Hello World Index".getBytes();
-            } else {
-                body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            }
-            response200Header(dos, body.length);
-            responseBody(dos, body);
             } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: /index.html\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
